38e85b382b41f2ba7728191a3a8d065a63028b96
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-15 22:02:02 +0530

    Update koupdater.py

--- userbot/modules/koupdater.py
+++ userbot/modules/koupdater.py
@@ -154,7 +154,7 @@ async def upstream(event):
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
 
-    ac_br = repo.active_branch.name
+    ac_br = "master"
     if ac_br != KOUPSTREAM_REPO_BRANCH:
         await event.edit(
             f"**Looks like you are using your own custom branch: ({ac_br}). \n"
