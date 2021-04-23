04fd83ea398ac7da5edd0a06431d5e91ecd1cd54
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-23 23:16:47 +0530

    hack: minimal changes

--- userbot/modules/hack.py
+++ userbot/modules/hack.py
@@ -45,7 +45,7 @@ async def hack_func(event):
         "**User Data Upload Completed:** Target's User Data Stored "
         "at `downloads/victim/telegram-authuser.data.sql`",
     ]
-    hecked = (f"**Targeted Account Hacked**\n\n**Pay 69$ To **{DEFAULTUSER}\n**To Remove This Hack**")
+    hecked = (f"**Targeted Account Hacked**\n\nPay 69$ To **{DEFAULTUSER}\n**To Remove This Hack")
     max_ani = len(animation_chars)
     for i in range(max_ani):
         await sleep(2)
