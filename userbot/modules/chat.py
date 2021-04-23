6b2f974d38c882deb0ba58d9f8e2649513c1cb44
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-23 23:04:41 +0530

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -153,51 +153,7 @@ async def sedNinjaToggle(event):
         regexNinja = False
         await event.edit("`Successfully disabled ninja mode for Regexbot.`")
         await sleep(1)
-        await event.delete()
-
-
-@register(outgoing=True, pattern="^.getid$")
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(2)
-    if input_str:
-        try:
-            p = await event.client.get_entity(input_str)
-        except Exception as e:
-            return await edit_delete(event, f"`{str(e)}`", 5)
-        try:
-            if p.first_name:
-                return await edit_or_reply(
-                    event, f"The id of the user `{input_str}` is `{p.id}`"
-                )
-        except Exception:
-            try:
-                if p.title:
-                    return await edit_or_reply(
-                        event, f"The id of the chat/channel `{p.title}` is `{p.id}`"
-                    )
-            except Exception as e:
-                LOGS.info(str(e))
-        await edit_or_reply(event, "`Either give input as username or reply to user`")
-    elif event.reply_to_msg_id:
-        await event.get_input_chat()
-        r_msg = await event.get_reply_message()
-        if r_msg.media:
-            bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await edit_or_reply(
-                event,
-                f"**Current Chat ID : **`{str(event.chat_id)}`\n**From User ID: **`{str(r_msg.sender_id)}`\n**Media File ID: **`{bot_api_file_id}`",
-            )
-        else:
-            await edit_or_reply(
-                event,
-                f"**Current Chat ID : **`{str(event.chat_id)}`\n**From User ID: **`{str(r_msg.sender_id)}`",
-            )
-    else:
-        await edit_or_reply(event, f"**Current Chat ID : **`{str(event.chat_id)}`")
-
-        
+        await event.delete()       
         
 CMD_HELP.update(
     {
