b25d9775d163940d368f9f0da770190592fabcb2
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-24 22:58:13 +0530

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -54,6 +54,19 @@ async def run_cmd(cmd: list) -> tuple[bytes, bytes]:
     e_resp = err.strip()
     return t_resp, e_resp
 
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
 
 async def edit_or_reply(
     event,
