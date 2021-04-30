f16d869b18d00e2a514cf32c29226b4afee1bc12
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-30 09:22:51 +0530

    Update sticklet.py

--- userbot/modules/sticklet.py
+++ userbot/modules/sticklet.py
@@ -25,7 +25,7 @@ async def sticklet(event):
     font_file_name = event.pattern_match.group(1)
     if not font_file_name:
         font_file_name = ""
-    sticktext = event.pattern_match.group(1)
+    sticktext = event.pattern_match.group(2)
     if not sticktext:
         if event.reply_to_msg_id:
             reply_message = await event.get_reply_message()
@@ -46,7 +46,7 @@ async def sticklet(event):
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
-    FONT_FILE = ("Fizilion/resources/Aerospace.ttf", font_file_name)
+    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
@@ -71,3 +71,20 @@ async def sticklet(event):
         os.remove(FONT_FILE)
     except BaseException:
         pass
+
+
+async def get_font_file(client, channel_id, search_kw=""):
+    # first get the font messages
+    font_file_message_s = await client.get_messages(
+        entity=channel_id,
+        filter=InputMessagesFilterDocument,
+        # this might cause FLOOD WAIT,
+        # if used too many times
+        limit=None,
+        search=search_kw,
+    )
+    # get a random font from the list of fonts
+    # https://docs.python.org/3/library/random.html#random.choice
+    font_file_message = random.choice(font_file_message_s)
+    # download and return the file path
+    return await client.download_media(font_file_message)
