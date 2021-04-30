47b3b2dbce1d2fad33e1c0eaa601a1421d491b84
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-30 09:24:20 +0530

    Update sticklet.py

--- userbot/modules/sticklet.py
+++ userbot/modules/sticklet.py
@@ -25,7 +25,7 @@ async def sticklet(event):
     font_file_name = event.pattern_match.group(1)
     if not font_file_name:
         font_file_name = ""
-    sticktext = event.pattern_match.group(2)
+    sticktext = event.pattern_match.group(1)
     if not sticktext:
         if event.reply_to_msg_id:
             reply_message = await event.get_reply_message()
