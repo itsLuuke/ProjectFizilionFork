10b9dbf8e1aaf416ca2b696b981649913f5498c8
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-30 09:04:44 +0530

    Update stats.py

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -157,9 +157,11 @@ async def _(event):
         await sevent.edit(response.text)
         """Cleanup after completed"""
         await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
-    except TimeoutError:
+        except TimeoutError:
         return await event.edit("**Error:** @tgscanrobot **is not responding.**")
     await event.delete()
+        
+
         
 CMD_HELP.update({
     "stats":
