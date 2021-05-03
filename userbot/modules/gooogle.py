54a83a68efddc110675206325909928d826966a6
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-03 13:04:04 +0530

    Update gooogle.py

--- userbot/modules/gooogle.py
+++ userbot/modules/gooogle.py
@@ -5,7 +5,7 @@ from search_engine_parser.core.exceptions import NoResultsOrTrafficError
 from userbot.events import register
 from userbot import BOTLOG, BOTLOG_CHATID
 
-@register(outgoing=True, pattern=r"^.gooogle(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^.google(?: |$)(.*)")
 async def gsearch(q_event):
     """For .google command, do a Google search."""
     textx = await q_event.get_reply_message()
