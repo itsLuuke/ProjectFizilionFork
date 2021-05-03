9bbcfc53e2d485add27f232fdff4ce914472ccb8
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-03 10:01:36 +0530

    Update gooogle.py

--- userbot/modules/gooogle.py
+++ userbot/modules/gooogle.py
@@ -1,39 +1,40 @@
+import asyncio
 from search_engine_parser.core.engines.google import Search as GoogleSearch
 from userbot.events import register
 
+
 @register(outgoing=True, pattern=r"^\.gooogle(?: |$)(\d*)? ?(.*)")
-async def gsearch(gs):
-    await gs.edit("Processing ...")
-    query = gs.filtered_input_str
-    flags = gs.flags
-    page = int(flags.get("-p", 1))
-    limit = int(flags.get("-l", 5))
-    if gs.reply_to_message:
-        query = gs.reply_to_message.text
-    if not query:
-        await gs.err(text="Give a query or reply to a message to google!")
-        return
+await q_event.edit("`searching........`")
+    match = q_event.pattern_match.group(1)
+    page = re.findall(r"page=\d+", match)
     try:
-        g_search = GoogleSearch()
-        gresults = await g_search.async_search(query, page)
-    except Exception as e:
-        await gs.err(text=e)
-        return
-    output = ""
-    for i in range(limit):
+        page = page[0]
+        page = page.replace("page=", "")
+        match = match.replace("page=" + page[0], "")
+    except IndexError:
+        page = 1
+    search_args = (str(match), int(page))
+    gsearch = GoogleSearch()
+    try:
+        gresults = await gsearch.async_search(*search_args)
+    except Exception:
+        return await q_event.edit(
+            "`Error: Your query could not be found or it was flagged as unusual traffic.`"
+        )
+    msg = ""
+
+    for i in range(len(gresults["links"])):
         try:
             title = gresults["titles"][i]
             link = gresults["links"][i]
             desc = gresults["descriptions"][i]
-            output += f"[{title}]({link})\n"
-            output += f"`{desc}`\n\n"
+            msg += f"👉[{title}]({link})\n`{desc}`\n\n"
         except IndexError:
             break
-    output = f"**Google Search:**\n`{query}`\n\n**Results:**\n{output}"
-    await gs.edit_or_send_as_file(
-        text=output, caption=query, disable_web_page_preview=True
+    await q_event.edit(
+        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
     )
     if BOTLOG:
-        await gs.client.send_message(
+        await q_event.client.send_message(
             BOTLOG_CHATID, "Google Search query `" + match + "` was executed successfully"
         )
