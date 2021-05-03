cfcb926ff45907c07c627671a7aaf7848f3dfdcb
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-03 13:04:35 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -155,43 +155,6 @@ async def moni(event):
     c_to_val = round(c_from_val * response["rates"][c_to], 2)
     await event.edit(f"`{c_from_val} {c_from} = {c_to_val} {c_to}`")
 
-@register(outgoing=True, pattern=r"^\.google(?: |$)(\d*)? ?(.*)")
-async def gsearch(q_event):
-    await q_event.edit("`searching........`")
-    match = q_event.pattern_match.group(1)
-    page = re.findall(r"page=\d+", match)
-    try:
-        page = page[0]
-        page = page.replace("page=", "")
-        match = match.replace("page=" + page[0], "")
-    except IndexError:
-        page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    try:
-        gresults = await gsearch.async_search(*search_args)
-    except Exception:
-        return await q_event.edit(
-            "`Error: Your query could not be found or it was flagged as unusual traffic.`"
-        )
-    msg = ""
-
-    for i in range(len(gresults["links"])):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"👉[{title}]({link})\n`{desc}`\n\n"
-        except IndexError:
-            break
-    await q_event.edit(
-        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
-    )
-    if BOTLOG:
-        await q_event.client.send_message(
-            BOTLOG_CHATID, "Google Search query `" + match + "` was executed successfully"
-        )
-
 
 @register(outgoing=True, pattern=r"^\.wiki(?: |$)(.*)")
 async def wiki(wiki_q):
