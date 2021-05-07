f64a7edb74e6302c9ea92da03d5f8578a49dd948
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-07 09:27:24 +0530

    Update gban_sql_helper.py

--- userbot/modules/sql_helper/gban_sql_helper.py
+++ userbot/modules/sql_helper/gban_sql_helper.py
@@ -12,7 +12,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from sqlalchemy import Column, String
 
-from userbot.plugins.sql_helper import BASE, SESSION
+from userbot.modules.sql_helper import BASE, SESSION
 
 
 class GBan(BASE):
