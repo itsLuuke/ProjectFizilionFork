3659d47860fb52aa5a3521e435900a3f9be3debe
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-09 21:41:18 +0530

    Update trd.py

--- userbot/modules/trd.py
+++ userbot/modules/trd.py
@@ -14,5 +14,5 @@ T_R_D = [
 async def truthrdare(trd):
     """Truth or Dare"""
     await trd.edit("`Choosing Name...`")
-    await trd.sleep(3)
+    await sleep(3)
     await trd.edit(choice(T_R_D))
