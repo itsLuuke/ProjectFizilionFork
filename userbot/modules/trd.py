83fe96e6714bea6c7dd30d3ad022c704969453ed
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-05-09 21:59:32 +0530

    Update trd.py

--- userbot/modules/trd.py
+++ userbot/modules/trd.py
@@ -4,21 +4,21 @@ from random import choice
 from userbot.events import register
 
 T_R_D = [
-    "**Prajwal**",
-    "**Vinaya**",
-    "**Sharan**",
-    "**Srinidhi**",
+    "`Prajwal`",
+    "`Vinaya`",
+    "`Sharan`",
+    "`Srinidhi`",
 ]
 
 @register(outgoing=True, pattern="^.trd$")
 async def truthrdare(trd):
     """Truth or Dare"""
-    await trd.edit("`Choosing Name...")
+    await trd.edit("`Choosing Name...`")
     await sleep(1.5)
     await trd.edit("`..............`")
     await sleep(1.5)
     msg = await trd.edit("`Name is.....`")
     await sleep(3)
     await trd.delete()
-    await msg.reply("Name: " + choice(T_R_D))
+    await msg.reply("**Name:** " + choice(T_R_D))
     
