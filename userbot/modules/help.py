821f08cacfeaaba9f3222262ff5e806d3af65c51
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-10 18:28:32 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -62,34 +62,3 @@ async def help(event):
                     event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                 )
                 await event.delete()
-  #  """ For .help command,"""
-   # args = event.pattern_match.group(1).lower()
-    # Prevent Channel Bug to get any information and command from all modules
-  #  if event.is_channel and not event.is_group:
-  #      await event.edit("`Help command isn't permitted on channels`")
-  #      return
-  #  if args:
-  #      if args in CMD_HELP:
-  #          msg=await event.edit(str(CMD_HELP[args]))
-  #          await sleep(45)
-         
- #       else:
- #           msg=await event.edit("Please specify a valid module name.")
- #           await sleep(15)   
- #   else:
- #       final = "**List of all loaded module(s)**\n\
- #       \nSpecify which module do you want help for! \
- #       \n**Usage:** `.help` <module name>\n\n"
- #       temp = "".join(str(i) + " " for i in CMD_HELP)
- #       temp = sorted(temp.split())
- #       for i in temp:
- #           final += "`" + str(i)
- #           final += "`\t\t\t•\t\t\t"
- #       msg=await event.edit(f"{final[:-5]}")
- #       await sleep(45)
-        
-#    if TIMEOUT:
-#        await msg.delete()
-#                
-#########
-##########
