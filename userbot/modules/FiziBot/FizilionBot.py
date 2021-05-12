fc9400a2cf191c80c6967ad55a196c66a66a3c2a
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-12 18:49:24 +0400

    Update FizilionBot.py

--- userbot/modules/FiziBot/FizilionBot.py
+++ userbot/modules/FiziBot/FizilionBot.py
@@ -3,11 +3,17 @@ import asyncio
 import os
 import requests
 from telethon import events
-from userbot import inlinebot
+from userbot import BOT_TOKEN, API_KEY, API_HASH, LOGS
 # start-others
+try:
+    inlinebot = TelegramClient("inlinebot", API_KEY, API_HASH).start(bot_token=BOT_TOKEN)
+except Exception as e:
+    LOGS.info("INLINEBOT FAILED.")
+    LOGS.info("INLINEBOT is quiting...")
+    LOGS.info(str(e))
+    exit()
 
-
-@inlinebot.on(events.NewMessage(pattern="^/start"))  # pylint: disable=oof
+@inlinebot.on(events.NewMessage(pattern="/start"))  # pylint: disable=oof
 async def start_all(event):
     if event.chat_id == OWNER_ID:
         return
@@ -17,7 +23,7 @@ async def start_all(event):
 # start-owner
 
 
-@inlinebot.on(events.NewMessage(pattern="^/start",
+@inlinebot.on(events.NewMessage(pattern="/start",
                             from_users=OWNER_ID))  # pylint: disable=oof
 async def owner(event):
     await inlinebot.send_message(event.chat_id, "TEST=OWNER")
