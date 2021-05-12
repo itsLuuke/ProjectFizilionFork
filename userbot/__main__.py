5702dc41819e5cf7e6680513523c0a73544f90ea
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-12 19:13:27 +0400

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -33,6 +33,33 @@ LOGS.info("You are running Project Fizilion")
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
     "If you need assistance, head to https://t.me/ProjectFizilionChat")
+OWNER_ID = 1391975600
+# start-others
+try:
+    LOGS.info("INITIATING BOT....")
+    inlinebot = TelegramClient("inlinebot", API_KEY, API_HASH).start(bot_token=BOT_TOKEN)
+except Exception as e:
+    LOGS.info("INLINEBOT FAILED.")
+    LOGS.info("INLINEBOT is quiting...")
+    LOGS.info(str(e))
+    exit()
+
+@inlinebot.on(events.NewMessage(pattern="/start"))  # pylint: disable=oof
+async def start_all(event):
+    if event.chat_id == OWNER_ID:
+        return
+    await inlinebot.send_message(event.chat_id, "TEST=NOTOWNER")
+      
+
+# start-owner
+
+
+@inlinebot.on(events.NewMessage(pattern="/start",
+                            from_users=OWNER_ID))  # pylint: disable=oof
+async def owner(event):
+    await inlinebot.send_message(event.chat_id, "TEST=OWNER")
+
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
