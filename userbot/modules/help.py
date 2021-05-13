22b0bc8cbf993a8690630f39d4ff1de8bdee43c9
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-13 09:27:02 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,20 +25,20 @@ async def help(event):
         else:
             msg=await event.edit("Please specify a valid module name.")
             await sleep(15)   
-        else:
-            tgbotusername = BOT_USERNAME
-            help_string = "Userbot Helper for TEST to reveal all the commands of TEST"
-            try:
-                results = await bot.inline_query(  # pylint:disable=E0602
+    else:
+        tgbotusername = BOT_USERNAME
+        help_string = "Userbot Helper for TEST to reveal all the commands of TEST"
+        try:
+            results = await bot.inline_query(  # pylint:disable=E0602
                  #   tgbotusername, help_string
                  "@senpaitorrentleechbot", help_string  
                 )
-                await results[0].click(
+            await results[0].click(
                     event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                 )
                # await event.answer([results])
-                await event.delete()
-            except Exception as e:
+            await event.delete()
+        except Exception as e:
                LOGS.info(str(e))
          #   except BaseException:
           #      await event.edit(
