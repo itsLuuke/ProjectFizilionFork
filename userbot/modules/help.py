7fbbf49083839d2dac5d45d9388c1fbfdb2d075a
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-13 08:55:29 +0000

    Create help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -5,7 +5,7 @@
 #
 """ Userbot help command """
 
-from userbot import CMD_HELP, TIMEOUT, BOT_USERNAME, CMD_LIST, LOGS, bot
+from userbot import CMD_HELP, TIMEOUT, BOT_USERNAME, CMD_LIST, bot
 from userbot.events import register
 from asyncio import sleep
 
@@ -55,7 +55,7 @@ async def help(event):
        #         await event.edit(input_str + " is not a valid plugin!")
        # else:
             tgbotusername = BOT_USERNAME
-            help_string = f"""`Userbot Helper for TEST to reveal all the commands of `**TEST**\n\n"""
+            help_string = "Userbot Helper for TEST to reveal all the commands of TEST"
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                  #   tgbotusername, help_string
@@ -64,7 +64,7 @@ async def help(event):
                 await results[0].click(
                     event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                 )
-                await event.answer([results])
+               # await event.answer([results])
                 await event.delete()
             except Exception as e:
                LOGS.info(str(e))
