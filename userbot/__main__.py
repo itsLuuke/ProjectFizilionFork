3ba382953fcaca66000d1d164ebfa083dd970494
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-13 09:53:32 +0000

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -4,17 +4,52 @@
 # you may not use this file except in compliance with the License.
 #
 """ Userbot start point """
+import platform
+import shutil
+import sys
+import pip
+import distro
+import time
+import os
 import re
+import time
 from importlib import import_module
 from sys import argv
 from telethon import TelegramClient, events, Button
 import telethon.utils
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS, bot, BOT_TOKEN, BOT_USERNAME, API_KEY, API_HASH
+from userbot import LOGS, bot, BOT_TOKEN, BOT_USERNAME, API_KEY, API_HASH, ALIVE_LOGO, USERBOT_VERSION, StartTime
 from userbot.modules import ALL_MODULES
+from platform import python_version, uname
+from shutil import which
+import psutil
+from git import Repo
+from telethon import __version__, version
 
+uptime = await get_readable_time((time.time() - StartTime))
+ALIVE_TEXT = (
+        f"`===============================`\n"
+        f"**FIZILION IS UP AND RUNNING...**\n"
+        f"`=============================== `\n"
+        f"**[OS Info]:**\n"
+        f"•`Platform Type   : {os.name}`\n"
+        f"•`Distro          : {distro.name(pretty=False)} {distro.version(pretty=False, best=False)}`\n"
+        f"`===============================`\n"
+        f"**[PYPI Module Versions]:**\n"
+        f"•`Python         : v{python_version()} `\n"   
+        f"•`Telethon       : v{version.__version__} `\n"
+        f"•`PIP            : v{pip.__version__} `\n"
+        f"`===============================`\n"
+        f"**[MISC Info]:**\n"
+        f"•`User           : {DEFAULTUSER} `\n"
+        f"•`Running on     : {repo.active_branch.name} `\n"
+        f"•`Loaded modules : {len(modules)} `\n"
+        f"•`Fizilion       : {USERBOT_VERSION} `\n"
+        f"•`Bot Uptime     : {uptime} `\n"
+        f"`===============================`\n"
 
+    )
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
              '\n Tip: Use Country Code along with number.' \
              '\n or check your phone number and try again !'
@@ -108,6 +143,7 @@ async def _(e):
 @inlinebot.on(events.callbackquery.CallbackQuery(data=re.compile(b"back(.*)")))
 async def _(e):
     await back(e)    
+    
 @inlinebot.on(events.InlineQuery)
 async def handler(event):
 #    builder = event.builder
@@ -120,9 +156,21 @@ async def handler(event):
     builder = event.builder
     query = event.text
     if event.query.user_id == bot.uid:
+      result = builder.photo(
+                file=ALIVE_LOGO,
+                text=ALIVE_TEXT,
+                buttons=[
+                    [
+                        custom.Button.inline("BUTTON1", data="req"),
+                        custom.Button.inline("BUTTON2", data="chat"),
+                    ],
+                    [custom.Button.inline("BUTTTON3", data="heheboi")],
+                    [custom.Button.inline("BUTTON4", data="pmclick")],
+                ],
+            )
       r1 = builder.article('1. TEST', text="TEST HELP")
       r2 = builder.article('2. TEST', text="TEST HELP2")
-      await event.answer([r1, r2])
+      await event.answer([result, r1, r2])
     else:
       notmaster = builder.article('Not for you boss', text='You are not my master you bastard')
       await event.answer([notmaster])
