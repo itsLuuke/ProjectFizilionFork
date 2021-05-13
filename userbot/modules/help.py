28f432a5c31246d25650938b8a0ad4051c94f344
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-13 09:24:55 +0000

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -12,48 +12,20 @@ from asyncio import sleep
 @register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
 async def help(event):
    
-   if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-       tgbotusername = BOT_USERNAME
-       input_str = event.pattern_match.group(1)
-       if tgbotusername is None or input_str == "text":
-            string = ""
-            for i in CMD_HELP:
-                string += "EMOJITEST" + " " + i + " " + "EMOPJITEST2" + "\n"
-                for iter_list in CMD_HELP[i]:
-                    string += "    `" + str(iter_list) + "`"
-                    string += "\n"
-                string += "\n"
-            if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await tgbot.send_file(
-                        event.chat_id,
-                       out_file,
-                       force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id,
-                    )
-                    await event.delete()
-            else:
-                await event.edit(string)
-       elif input_str:
-            if input_str in CMD_LIST:
-                string = "**Commands available in {}** \n\n".format(input_str)
-                if input_str in CMD_HELP:
-                    for i in CMD_HELP[input_str]:
-                        string += i
-                    string += "\n\n**TEST1**"
-                    await event.edit(string)
-                else:
-                    for i in CMD_LIST[input_str]:
-                        string += "    " + i
-                        string += "\n"
-                    string += "\n**TEST2**"
-                    await event.edit(string)
-            else:
-                await event.edit(input_str + " is not a valid plugin!")
-       else:
+    args = event.pattern_match.group(1).lower()
+    # Prevent Channel Bug to get any information and command from all modules
+    if event.is_channel and not event.is_group:
+        await event.edit("`Help command isn't permitted on channels`")
+        return
+    if args:
+        if args in CMD_HELP:
+            msg=await event.edit(str(CMD_HELP[args]))
+            await sleep(45)
+         
+        else:
+            msg=await event.edit("Please specify a valid module name.")
+            await sleep(15)   
+        else:
             tgbotusername = BOT_USERNAME
             help_string = "Userbot Helper for TEST to reveal all the commands of TEST"
             try:
