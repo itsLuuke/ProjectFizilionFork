92689195b76d0fb17eed697f35f548840ff4275a
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-21 10:55:23 +0000

    Update anidl.py

--- userbot/modules/anidl.py
+++ userbot/modules/anidl.py
@@ -34,7 +34,7 @@ async def animedl(event):
     title5 = search[4].title
     url5 = search[4].url
 
-    await event.edit(
+    await event.reply(
         f"<b><u>test</b></u> \n\n\n<b>Title</b>:-  <code>{title1}</code> \n<b>URL Link</b>:- {url1}\n\n<b>Title</b>:-  <code>{title2}</code> \n<b>URL Link</b>:- {url2}\n\n<b>Title</b>:-  <code>{title3}</code>\n<b>URL Link</b>:- {url3}\n\n<b>Title</b>:-  <code>{title4}</code> \n<b>URL Link</b>:- {url4}\n\n<b>Title</b>:-  <code>{title5}</code> \n<b>URL Link</b>:- {url5}\n\n<b>Enjoy</b>",
         parse_mode="HTML",
     )
